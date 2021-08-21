2430309f09e019aed993fb0ecfc5dec52c401e84
Bowarc <yuyeaaaahhhh@laposte.net>
2021-08-21 22:02:08 +0200

    4th commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 #![allow(unused)]
-use bevy::{prelude::*, sprite::collide_aabb::collide};
+use bevy::prelude::{App, Command};
+use bevy_retrograde::prelude::RetroPlugins;
 fn main() {
-    println!("Hello, world!");
+    App.build()
+        .add_plugins(RetroPlugins)
+        .add_startup_system(setup.system())
+        .run();
 }
+
+fn setup(mut commands: Command, asset_server: Res<AssetServer>) {}
