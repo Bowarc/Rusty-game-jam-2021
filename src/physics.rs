bb44a7d5d0225c71650ce5b00e4a7ac92dc3ca8e
Bowarc <yuyeaaaahhhh@laposte.net>
2021-08-22 22:35:07 +0200

    adding world collision methods

--- src/physics.rs
+++ src/physics.rs
@@ -12,3 +12,87 @@ use bevy::prelude::Vec3;
 //         pt
 //     }
 // }
+
+pub struct Hitbox {
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+}
+
+impl Hitbox {
+    pub fn new(x: f32, y: f32, w: f32, h: f32) -> Self {
+        Hitbox {
+            x: x,
+            y: y,
+            w: w,
+            h: h,
+        }
+    }
+}
+
+pub struct Circle {
+    x: f32,
+    y: f32,
+    radius: f32,
+}
+
+pub struct WorldCollision;
+
+impl WorldCollision {
+    pub fn two_rect(hb1: Hitbox, hb2: Hitbox) -> bool {
+        let is_collision = hb1.x < hb2.x + hb2.w
+            && hb1.x + hb1.h > hb2.x
+            && hb1.y < hb1.y + hb2.h
+            && hb1.h + hb1.y > hb2.y;
+        is_collision
+    }
+    pub fn two_circle(circle1: Circle, circle2: Circle) -> bool {
+        // https://developer.mozilla.org/fr/docs/Games/Techniques/2D_collision_detection
+        let dx = circle1.x - circle2.x;
+        let dy = circle1.y - circle2.y;
+        let distance = (dx * dx + dy * dy).sqrt();
+
+        distance < circle1.radius + circle2.radius
+    }
+    pub fn world_collision(
+        entity_hitbox: Hitbox,
+        wanted_pos: (f32, f32),
+        collide_list: Vec<Hitbox>, //any object that has an hitbow
+    ) -> Hitbox {
+        let mut wanted_pos = wanted_pos.clone();
+        for hb in collide_list {
+            let dx = Hitbox::new(
+                wanted_pos.0,
+                entity_hitbox.y,
+                entity_hitbox.w,
+                entity_hitbox.h,
+            );
+            let dy = Hitbox::new(
+                entity_hitbox.x,
+                wanted_pos.1,
+                entity_hitbox.w,
+                entity_hitbox.h,
+            );
+            if WorldCollision::two_rect(hb, dx) {
+                if entity_hitbox.x - wanted_pos.0 > 0.0 {
+                    wanted_pos.0 = hb.x + hb.w;
+                    // println!("Collided left");
+                } else {
+                    wanted_pos.0 = hb.x - entity_hitbox.w;
+                    // println!("Collided right");
+                }
+            }
+            if WorldCollision::two_rect(hb, dy) {
+                if entity_hitbox.y - wanted_pos.1 > 0.0 {
+                    // println!("Collided up");
+                    wanted_pos.1 = hb.y + hb.h;
+                } else {
+                    // println!("Collided down");
+                    wanted_pos.1 = hb.y - entity_hitbox.h;
+                }
+            }
+        }
+        Hitbox::new(wanted_pos.0, wanted_pos.1, entity_hitbox.w, entity_hitbox.h)
+    }
+}
