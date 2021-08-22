38753b7c73cd3e1928fcc69365dcd9883f915ff1
Bowarc <yuyeaaaahhhh@laposte.net>
2021-08-23 01:50:03 +0200

    fixed big mistake xd

--- src/map.rs
+++ src/map.rs
@@ -1,10 +1,22 @@
-#[derive(Default)]
-pub struct Map {
-    raw_map: Vec<Vec<i32>>,
-}
+use bevy::prelude::*;
+//use bevy_retrograde::prelude::*;
 
-impl Map {
-    fn load_new_map(&mut self, map_name: String) {
-        println!("Loading map: {}", map_name);
+/*pub struct map_manager {
+    map_raw_file: Vec2,
+    // bloc_list: Vec<Vec<bloc::Bloc,
+impl map_manager {
+    pub fn new() -> Self {
+    }
+    pub fn load_map(&mut self) {}
+}*/
+
+pub struct MapPlugin;
+
+impl Plugin for MapPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.insert_resource(WindowDescriptor {
+            title: "Rusty caves".into(),
+            ..Default::default()
+        });
     }
 }
