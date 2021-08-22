54cb3773d399504d39362441d84dfcad2d02fa32
Eragon <sam.vzh@netc.fr>
2021-08-22 13:18:41 +0200

    Adding a player and moving it

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-#[deny(unsafe_code)]
+#![deny(unsafe_code)]
 use bevy::prelude::*;
 use bevy_retrograde::prelude::*;
 
 mod item;
 mod player;
 
-use player::PlayerPlugin;
+use player::{Player, PlayerPlugin, SpriteAnimFrame};
+
+#[derive(StageLabel, Debug, Eq, Hash, PartialEq, Clone)]
+struct GameStage;
 
 fn main() {
 	App::build()
@@ -13,12 +16,17 @@ fn main() {
 			title: "Rusty caves".into(),
 			..Default::default()
 		})
+		.add_plugins(RetroPlugins)
 		.add_plugin(PlayerPlugin)
 		.add_startup_system(setup.system())
 		.run();
 }
 
-fn setup(mut commands: Commands/*, asset_server: Res<AssetServer>*/) {
+fn setup(
+	mut commands: Commands,
+	asset_server: Res<AssetServer>,
+	mut sprite_sheet_assets: ResMut<Assets<SpriteSheet>>,
+) {
 	// spawn the camera
 	commands.spawn_bundle(CameraBundle {
 		camera: Camera {
@@ -30,7 +38,44 @@ fn setup(mut commands: Commands/*, asset_server: Res<AssetServer>*/) {
 		..Default::default()
 	});
 
-	// TODO: Load the assets
+	// Load the assets
+	let red_radish = asset_server.load("redRadishSheet.png"); // We should use our own sprite sheet
 
 	// TODO: Load the map
+
+	// Spawn the player and assign sprite
+	commands
+		.spawn_bundle(SpriteSheetBundle {
+			sprite_bundle: SpriteBundle {
+				image: red_radish.clone(),
+				sprite: Sprite {
+					pixel_perfect: false,
+					..Default::default()
+				},
+				transform: Transform::from_xyz(0., -50., 0.),
+				..Default::default()
+			},
+			sprite_sheet: sprite_sheet_assets.add(SpriteSheet {
+				grid_size: UVec2::splat(16),
+				tile_index: 0,
+			}),
+		})
+		.insert(SpriteAnimFrame(0))
+		.insert(TesselatedCollider {
+			image: red_radish.clone(),
+			tesselator_config: TesselatedColliderConfig {
+				vertice_separation: 0.,
+				..Default::default()
+			},
+			..Default::default()
+		})
+		.insert(RigidBody::Dynamic)
+		.insert(RotationConstraints::lock())
+		.insert(PhysicMaterial {
+			friction: 0.,
+			restitution: 0.,
+			..Default::default()
+		})
+		.insert(Velocity::from_linear(Vec3::default()))
+		.insert(Player::new("Bob".to_string()));
 }
--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,11 @@
 use bevy::prelude::*;
+use bevy_retrograde::prelude::*;
 
 use crate::item::Item;
 
 const PLAYER_INVENTORY_SIZE: i32 = 10;
+
+pub struct SpriteAnimFrame(pub usize);
 
 pub struct Player {
 	pub inventory: Vec<Item>,
@@ -22,12 +25,81 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
 	fn build(&self, app: &mut AppBuilder) {
-		app.add_startup_stage("game_actors", SystemStage::single(player_spawn.system()))
-			/*.add_system(player_movement.system())*/;
+		app
+		.add_system(player_movement.system());
 	}
 }
 
-fn player_spawn(mut commands: Commands) {}
+// Change the player velocity and animation based on the key press.
+fn player_movement(
+	mut frame: Local<u8>,
+	keyboard_input: Res<Input<KeyCode>>,
+	mut query: Query<&mut Velocity, With<Player>>,
+	mut q: Query<(&Handle<SpriteSheet>, &mut SpriteAnimFrame), With<Handle<Image>>>,
+	mut sprite_sheet_assets: ResMut<Assets<SpriteSheet>>,
+) {
+	const SPEED: f32 = 30.;
+	*frame = frame.wrapping_add(1);
 
-/*fn player_movement(keyboard_input: Res<Input<KeyCode>>, mut q: Query<&mut Velocity, With<Player>) {
-}*/
+	for mut velocity in query.iter_mut() {
+		let mut direction = Vec3::new(0., 0., 0.);
+
+		if keyboard_input.pressed(KeyCode::Left) {
+			direction += Vec3::new(-SPEED, 0., 0.);
+			let frames = [12, 13, 14, 15];
+			if *frame % 10 == 0 {
+				*frame = 0;
+				for (sprite_sheet_handle, mut frame) in q.iter_mut() {
+					if let Some(sprite_sheet) = sprite_sheet_assets.get_mut(sprite_sheet_handle) {
+						frame.0 = frame.0.wrapping_add(1);
+						sprite_sheet.tile_index = frames[frame.0 % frames.len()];
+					}
+				}
+			}
+		}
+
+		if keyboard_input.pressed(KeyCode::Right) {
+			direction += Vec3::new(SPEED, 0., 0.);
+			let frames = [4, 5, 6, 7];
+			if *frame % 10 == 0 {
+				*frame = 0;
+				for (sprite_sheet_handle, mut frame) in q.iter_mut() {
+					if let Some(sprite_sheet) = sprite_sheet_assets.get_mut(sprite_sheet_handle) {
+						frame.0 = frame.0.wrapping_add(1);
+						sprite_sheet.tile_index = frames[frame.0 % frames.len()];
+					}
+				}
+			}
+		}
+
+		if keyboard_input.pressed(KeyCode::Up) {
+			direction += Vec3::new(0., -SPEED, 0.);
+			let frames = [8, 9, 10, 11];
+			if *frame % 10 == 0 {
+				*frame = 0;
+				for (sprite_sheet_handle, mut frame) in q.iter_mut() {
+					if let Some(sprite_sheet) = sprite_sheet_assets.get_mut(sprite_sheet_handle) {
+						frame.0 = frame.0.wrapping_add(1);
+						sprite_sheet.tile_index = frames[frame.0 % frames.len()];
+					}
+				}
+			}
+		}
+
+		if keyboard_input.pressed(KeyCode::Down) {
+			direction += Vec3::new(0., SPEED, 0.);
+			let frames = [0, 1, 2, 3];
+			if *frame % 10 == 0 {
+				*frame = 0;
+				for (sprite_sheet_handle, mut frame) in q.iter_mut() {
+					if let Some(sprite_sheet) = sprite_sheet_assets.get_mut(sprite_sheet_handle) {
+						frame.0 = frame.0.wrapping_add(1);
+						sprite_sheet.tile_index = frames[frame.0 % frames.len()];
+					}
+				}
+			}
+		}
+
+		*velocity = Velocity::from_linear(direction);
+	}
+}
