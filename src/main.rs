e6c450a9377ec554936e3b7f51f20cbafc3b1eac
Bowarc <yuyeaaaahhhh@laposte.net>
2021-08-22 18:43:51 +0200

    fixed 8direction movement

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 use bevy_retrograde::prelude::*;
 
 mod item;
+mod physics;
 mod player;
 
 use player::{Player, PlayerPlugin, SpriteAnimFrame};
@@ -11,71 +12,71 @@ use player::{Player, PlayerPlugin, SpriteAnimFrame};
 struct GameStage;
 
 fn main() {
-	App::build()
-		.insert_resource(WindowDescriptor {
-			title: "Rusty caves".into(),
-			..Default::default()
-		})
-		.add_plugins(RetroPlugins)
-		.add_plugin(PlayerPlugin)
-		.add_startup_system(setup.system())
-		.run();
+    App::build()
+        .insert_resource(WindowDescriptor {
+            title: "Rusty caves".into(),
+            ..Default::default()
+        })
+        .add_plugins(RetroPlugins)
+        .add_plugin(PlayerPlugin)
+        .add_startup_system(setup.system())
+        .run();
 }
 
 fn setup(
-	mut commands: Commands,
-	asset_server: Res<AssetServer>,
-	mut sprite_sheet_assets: ResMut<Assets<SpriteSheet>>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut sprite_sheet_assets: ResMut<Assets<SpriteSheet>>,
 ) {
-	// spawn the camera
-	commands.spawn_bundle(CameraBundle {
-		camera: Camera {
-			size: CameraSize::FixedHeight(100),
-			background_color: Color::new(0.2, 0.2, 0.2, 1.0),
-			..Default::default()
-		},
-		transform: Transform::from_xyz(0., -50., 0.),
-		..Default::default()
-	});
+    // spawn the camera
+    commands.spawn_bundle(CameraBundle {
+        camera: Camera {
+            size: CameraSize::FixedHeight(100),
+            background_color: Color::new(0.2, 0.2, 0.2, 1.0),
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(0., -50., 0.),
+        ..Default::default()
+    });
 
-	// Load the assets
-	let red_radish = asset_server.load("redRadishSheet.png"); // We should use our own sprite sheet
+    // Load the assets
+    let red_radish = asset_server.load("redRadishSheet.png"); // We should use our own sprite sheet
 
-	// TODO: Load the map
+    // TODO: Load the map
 
-	// Spawn the player and assign sprite
-	commands
-		.spawn_bundle(SpriteSheetBundle {
-			sprite_bundle: SpriteBundle {
-				image: red_radish.clone(),
-				sprite: Sprite {
-					pixel_perfect: false,
-					..Default::default()
-				},
-				transform: Transform::from_xyz(0., -50., 0.),
-				..Default::default()
-			},
-			sprite_sheet: sprite_sheet_assets.add(SpriteSheet {
-				grid_size: UVec2::splat(16),
-				tile_index: 0,
-			}),
-		})
-		.insert(SpriteAnimFrame(0))
-		.insert(TesselatedCollider {
-			image: red_radish.clone(),
-			tesselator_config: TesselatedColliderConfig {
-				vertice_separation: 0.,
-				..Default::default()
-			},
-			..Default::default()
-		})
-		.insert(RigidBody::Dynamic)
-		.insert(RotationConstraints::lock())
-		.insert(PhysicMaterial {
-			friction: 0.,
-			restitution: 0.,
-			..Default::default()
-		})
-		.insert(Velocity::from_linear(Vec3::default()))
-		.insert(Player::new("Bob".to_string()));
+    // Spawn the player and assign sprite
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite_bundle: SpriteBundle {
+                image: red_radish.clone(),
+                sprite: Sprite {
+                    pixel_perfect: false,
+                    ..Default::default()
+                },
+                transform: Transform::from_xyz(0., -50., 0.),
+                ..Default::default()
+            },
+            sprite_sheet: sprite_sheet_assets.add(SpriteSheet {
+                grid_size: UVec2::splat(16),
+                tile_index: 0,
+            }),
+        })
+        .insert(SpriteAnimFrame(0))
+        .insert(TesselatedCollider {
+            image: red_radish.clone(),
+            tesselator_config: TesselatedColliderConfig {
+                vertice_separation: 0.,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(RigidBody::Dynamic)
+        .insert(RotationConstraints::lock())
+        .insert(PhysicMaterial {
+            friction: 0.,
+            restitution: 0.,
+            ..Default::default()
+        })
+        .insert(Velocity::from_linear(Vec3::default()))
+        .insert(Player::new("Bob".to_string()));
 }
---
+++ src/physics.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::Vec3;
+
+// pub fn normalise_vec3(vec: Vec3) -> Vec3 {
+//     Vec3::new(0., 0., 0.)
+// }
+
+// pub fn normalize_p2(pt: na::Point2<f32>) -> na::Point2<f32> {
+//     let d = ((pt.x * pt.x) + (pt.y * pt.y)).sqrt();
+//     if d != 0.0 {
+//         na::Point2::new(pt.x / d, pt.y / d)
+//     } else {
+//         pt
+//     }
+// }
