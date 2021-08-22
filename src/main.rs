c19229a2ea9e9244a894850b56ca2e7bb3609c57
Bowarc <yuyeaaaahhhh@laposte.net>
2021-08-22 19:57:33 +0200

    fixed main imports

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,58 @@
 #![deny(unsafe_code)]
 use bevy::{
-	app::AppExit,
-	prelude::*
+    app::AppExit,
+    prelude::{
+        App, AssetServer, Assets, Commands, EventWriter, Input, IntoSystem, KeyCode, Res, ResMut,
+        StageLabel, Transform, UVec2, Vec3,
+    },
 };
-use bevy_retrograde::prelude::*;
+
+// use bevy_ecs::system::into_system::IntoSystem;
+
+use bevy_tilemap::{chunk::LayerKind, prelude::TilemapBundle, Tilemap, TilemapLayer};
+// use bevy_retrograde::prelude::Visible;
+
+use bevy_retrograde::{
+    prelude::{
+        Camera, CameraBundle, CameraSize, Color, PhysicMaterial, RigidBody, RotationConstraints,
+        Sprite, SpriteBundle, SpriteSheet, SpriteSheetBundle, TesselatedCollider,
+        TesselatedColliderConfig, Velocity,
+    },
+    RetroPlugins,
+};
+
+use bevy_render::draw::Visible;
+use map::MapPlugin;
+use player::{Player, PlayerPlugin, SpriteAnimFrame};
 
 mod item;
+mod map;
 mod physics;
 mod player;
-mod map;
 
-use player::{Player, PlayerPlugin, SpriteAnimFrame};
-use map::MapPlugin;
+const TILEMAP_WIDTH: i32 = 60;
+const TILEMAP_HEIGHT: i32 = 60;
 
 #[derive(StageLabel, Debug, Eq, Hash, PartialEq, Clone)]
 struct GameStage;
 
 fn main() {
-	App::build()
-		.add_system(bevy::input::system::exit_on_esc_system.system())
-		.add_system(exit_system.system())
-		.add_plugins(RetroPlugins)
-		.add_plugin(MapPlugin)
-		.add_plugin(PlayerPlugin)
-		.add_startup_system(setup.system())
-		.run();
+    App::build()
+        .add_system(bevy::input::system::exit_on_esc_system.system())
+        .add_system(exit_system.system())
+        .add_plugins(RetroPlugins)
+        .add_plugin(MapPlugin)
+        .add_plugin(PlayerPlugin)
+        .add_startup_system(setup.system())
+        .run();
 }
 
 fn exit_system(mut exit: EventWriter<AppExit>, keyboard_input: Res<Input<KeyCode>>) {
-	if (keyboard_input.pressed(KeyCode::LControl) || keyboard_input.pressed(KeyCode::RControl)) && keyboard_input.pressed(KeyCode::Q) {
-		exit.send(AppExit);
-	}
+    if (keyboard_input.pressed(KeyCode::LControl) || keyboard_input.pressed(KeyCode::RControl))
+        && keyboard_input.pressed(KeyCode::Q)
+    {
+        exit.send(AppExit);
+    }
 }
 
 fn setup(
@@ -53,6 +75,34 @@ fn setup(
     let red_radish = asset_server.load("redRadishSheet.png"); // We should use our own sprite sheet
 
     // TODO: Load the map
+    let tilemap = Tilemap::builder()
+        .dimensions(TILEMAP_WIDTH as u32, TILEMAP_HEIGHT as u32)
+        // .chunk_dimensions(CHUNK_WIDTH, CHUNK_HEIGHT, 1)
+        .texture_dimensions(32, 32)
+        // .auto_chunk()
+        .auto_spawn(2, 2)
+        .add_layer(
+            TilemapLayer {
+                kind: LayerKind::Dense,
+                ..Default::default()
+            },
+            0,
+        )
+        // .texture_atlas(atlas_handle)
+        .finish()
+        .unwrap();
+
+    let tilemap_components = TilemapBundle {
+        tilemap,
+        visible: Visible {
+            is_visible: true,
+            is_transparent: true,
+        },
+        transform: Default::default(),
+        global_transform: Default::default(),
+    };
+    commands.spawn().insert_bundle(tilemap_components);
+    // .insert(Timer::from_seconds(0.075, true));
 
     // Spawn the player and assign sprite
     commands
